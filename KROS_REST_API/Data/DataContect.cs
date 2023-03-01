50486db48e976a535fdb64e7669dbfedcc063135
Michal <michal.zifcak12@gmail.com>
2023-03-01 18:38:46 +0100

    Add project files.

---
+++ KROS_REST_API/Data/DataContect.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KROS_REST_API.Data
+{
+    public class DataContect : DbContext
+    {
+        public DataContect(DbContextOptions<DataContect> options) : base(options) { }
+
+    }
+}
---
+++ KROS_REST_API/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace KROS_REST_API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Degree { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Email { get; set; }
+    }
+}
